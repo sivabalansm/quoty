dea1d3b004ac233e4d71ff746c2930a5adbb6915
Sivabalan Muthurajan Gaius <sivabalansm@yahoo.com>
2025-08-15 19:21:53 -0400

    Add firestore wip

--- src/script.js
+++ src/script.js
@@ -1,15 +1,70 @@
-class Database {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+// import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+const firebaseConfig = { };
+
+
+  // Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+const userCollection = collection(db, "quotes");
+const docs = await getDocs(userCollection);
+console.log(docs.forEach((doc) => {
+  console.log(`${doc.id} =>`, doc.data());
+}));
+
+
+
+
+class UserDatabase {
+  static instance = null;
+  constructor() {
+    if (UserDatabase.instance) {
+      return UserDatabase.instance;
+    }
+    UserDatabase.instance = this;
+  }
+
+  connect() {}
 
 }
 
 class QuoteRepository {
   #db = null;
+  #quotes = [];
 
-  constructor (db) {
+  constructor(db) {
     this.db = db;
   }
 
   getQuote() {}
-  addQuote(quote) {}
+
+  addQuote(quote) {
+    this.#db.addItem();
+  }
+
   removeQuote(quote) {}
 }
+
+class RepositoryFactory {
+  static createQuoteRepository() {
+    const db = new UserDatabase();
+    return new QuoteRepository(db);
+  }
+}
+
+
+const userQuoteRepo = RepositoryFactory.createQuoteRepository();
+
+// Get quote
+const addQuoteButton = document.getElementById("new-quote-button");
+addQuoteButton.addEventListener("click", () => {
+  const quoteInput = document.getElementById("new-quote-field");
+
+  userQuoteRepo.addQuote(quoteInput.value);
+  quoteInput.value = ''
+
+});
