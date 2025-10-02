01b065f83e2ba8657b99fbe174d6a86b457ddbec
Sivabalan Muthurajan (Velup) <sivabalansm@yahoo.com>
2025-10-01 20:23:17 -0400

    Add tty interactive to docker for ctrl-c to work

--- discord/start.sh
+++ discord/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -v $(pwd)/test/bot.cpp:/app/bot.cpp -e BOT_TOKEN="$BOT_TOKEN" quoty:latest
+docker run -it -v $(pwd)/test/bot.cpp:/app/bot.cpp -e BOT_TOKEN="$BOT_TOKEN" quoty:latest
